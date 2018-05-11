e3db3cd84332729c5023aedfc11a14ffe2fdddb9
Ahmed Matem Ahmed <ahmedmatem@gmail.com>
2018-05-11 23:43:45 +0100

    add ninject

--- AMA.SchoolManagementSystem.Data/MsSqlDbContext.cs
+++ AMA.SchoolManagementSystem.Data/MsSqlDbContext.cs
@@ -1,6 +1,7 @@
 ﻿namespace AMA.SchoolManagementSystem.Data
 {
     using AMA.SchoolManagementSystem.Data.Model;
+    using AMA.SchoolManagementSystem.Data.Model.Contracts;
     using Microsoft.AspNet.Identity.EntityFramework;
     using System;
     using System.Collections.Generic;
@@ -21,6 +22,38 @@
         public static MsSqlDbContext Create()
         {
             return new MsSqlDbContext();
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public override int SaveChanges()
+        {
+            this.ApplyAuditInfoRules();
+            return base.SaveChanges();
+        }
+
+        private void ApplyAuditInfoRules()
+        {
+            // Approach via @julielerman: http://bit.ly/123661P
+            foreach (var entry in
+                this.ChangeTracker.Entries()
+                    .Where(
+                        e =>
+                        e.Entity is IAuditable && ((e.State == EntityState.Added) || (e.State == EntityState.Modified))))
+            {
+                var entity = (IAuditable)entry.Entity;
+                if (entry.State == EntityState.Added && entity.CreatedOn == default(DateTime))
+                {
+                    entity.CreatedOn = DateTime.Now;
+                }
+                else
+                {
+                    entity.ModifiedOn = DateTime.Now;
+                }
+            }
         }
     }
 }
